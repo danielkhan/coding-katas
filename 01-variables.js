94cbe53d46b895a4bb022f3b76d1440891853753
dkhan <datenpunk@gmail.com>
2016-12-01 11:54:16 -0600

    Initial commit

---
+++ 01-variables.js
@@ -0,0 +1,47 @@
+// Declare a variable 'someNumber1'
+
+// Store the number 19 in someNumber1
+
+// Declare a variable 'someNumber2'
+
+// Store the number 23 in someNumber1
+
+// Log someNumber1 to the console
+
+// Log someNumber2 to the console
+
+// Log the sum of someNumber1 and someNumber2 to the console without using a new variable
+
+// Store the sum of someNumber1 and someNumber2 in a new variable named numberSum
+
+// Log numberSum to the console
+
+// Declare a function sumFunction1 that takes 2 parameters 'a' and 'b', sums them up and logs the result to the console
+
+// Call sumFunction
+
+// Declare a function sumFunction2 that takes 2 parameters and 'a' and 'b', sums them up and *returns* the result
+
+// Call sumFunction2 with a=12 and b=30
+
+// Call sumFunction2 again and log out the result on the console
+
+// Call sumFunction2 with a=12 and b=30 and add the 13 to it, store the result in a new variable sumResult1
+
+// Log out sumResult1
+
+// Increment sumResult1 by one
+
+// Log out sumResult1
+
+// Declare a variable 'someString1'
+
+// Store the string 'I like ' in someString1
+
+// Declare a variable 'someString2'
+
+// Store the string 'JavaScript' in someString2
+
+// Concatenate someString1 and someString2 and log out the result
+
+// Use sumFunction2 to achieve the same result and log out the result
---
+++ 02-arrays.js
@@ -0,0 +1,31 @@
+// Declare an array 'numberArray and store the numbers 1 to 10 in it
+
+// Declare an array 'stringArray' and store the strings 'one', 'two', 'three' up to 'ten' in it
+
+// Declare an array 'foxArray' and store the words contained in the sentence 'The quick brown fox jumps over the lazy dog' in it
+
+// Loop through numberArray and log out each value
+
+// Loop through stringArray and log out each value
+
+// Loop through foxArray and log out each value
+
+// Log the value of numberArray at index 3
+
+// Log the value of stringArray at index 7
+
+// Log out the string 'seven' in stringArray
+
+// Log out the number '3' in numberArray
+
+// Change 'fox' to cat in foxArray
+
+// Loop through foxArray and log out each value
+
+// Add a '!' to the end of foxArray 
+
+// Loop through foxArray and log out each value
+
+// Write a function 'changeFunction' that accepts an array, an index and a value, stores the passed variable at the given index and returns the array
+
+// Use changeFunction to change 'fox' back to 'cat' in foxArray and log out the result
