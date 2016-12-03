fbaa6b2b3cb457d61d448fe5b9057f546033ecd6
dkhan <datenpunk@gmail.com>
2016-12-03 13:52:18 -0600

    Initial commit for object kata

---
+++ 03-objects.js
@@ -0,0 +1,23 @@
+// Create an empty object and store it in the variable 'someObject1'
+
+// Add a key 'someKey' and a value 'someValue' to someObject1
+
+// Create a variable 'someVar1' and assign the string 'someKey' to it
+
+// Log out someObject1.someKey
+
+// Log out someObject1['someKey']
+
+// Log out Log out someObject1.someVar1 (it's intended that it's undefined)
+
+// Log out Log out someObject1['someVar1'] (it's intended that it's undefined)
+
+// Log out Log out someObject1[someVar1]
+
+// Add a key 'someKey2' to someObject1
+
+// Store all keys of someObject1 in a variable myKeys
+
+// Log out myKeys
+
+// Iterate over myKeys and print out key and value for each iteration
